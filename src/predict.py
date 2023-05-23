ff00f8eec0b8ae7f8c97176b589fff3d490a76c1
nikitaromanoov <nikitaromanoov@gmail.com>
2023-05-23 12:43:00 +0300

    Update ansible-kafka 09

--- src/predict.py
+++ src/predict.py
@@ -13,10 +13,10 @@ from kafka import KafkaProducer
 
 def  t_kafka(inp):
     vault = Vault(os.environ.get("ANSIBLE"))
-    for i in f"{data[4]}:{data[5]}":
-        print(i.upper())
     with open("redis.credit") as f:
         data = vault.load(f.read()).split(" ")
+    for i in f"{data[4]}:{data[5]}":
+        print(i.upper())
     producer = KafkaProducer(bootstrap_servers=f"{str(data[4])}:{str(data[5])}", api_version=(0, 10, 2))
     producer.send("kafka-pred", bytearray(str(inp), "utf-8"))
     producer.close()
